8f00c8e9fed32b04533f7e24605d03a4117ea3ba
Dhanush <danush2228s@gmail.com>
2023-02-12 18:29:47 +0530

    Feeded my new born baby with some codes.

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class HelloWorld
+
+{
+	
+     public static void main(String[] args)
+	
+       {
+		
+System.out.println("What's ur name--");
+		
+Scanner inp = new Scanner(System.in);
+		
+System.out.println("Hi "+ inp.nextLine());
+	
+}
+}
